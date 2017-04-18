5227d5071ad88a5553f93658e767a33038cf5e86
Zaiste <oh@zaiste.net>
2017-04-19 00:37:15 +0200

    feature: add `search` with pagination

--- test/index.js
+++ test/index.js
@@ -12,10 +12,22 @@ describe('baseRoute', () => {
   before(() => {
     graph = new FacebookGraph(config.accessToken);
   })
-  
+
   it('should return 4 posts from /zaiste.net', async () => {
     const posts = await graph.fetch(config.pages.zaistenet, 'posts', 4)
     expect(posts).to.be.a('array');
     expect(posts).to.have.lengthOf(4);
   });
+
+  it('should return 25 search results by default', async () => {
+    const pages = await graph.search({ q: 'geek', type: 'page', fields: 'name' })
+    expect(pages).to.be.a('array');
+    expect(pages).to.have.lengthOf(25);
+  });
+
+  it('should return specified number of search results', async () => {
+    const pages = await graph.search({ q: 'geek', type: 'page', fields: 'name' }, 33)
+    expect(pages).to.be.a('array');
+    expect(pages).to.have.lengthOf(33);
+  }).timeout(10500);
 });
